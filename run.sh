69eaa9147982ce2aeecc39db9a5b1e88f094091a
Mickael Salvas <mickael_salvas21@hotmail.com>
2023-05-16 16:09:05 -0400

    Added volume to docker run and added git ignore for storage volume

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name mssql-docker -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Omega123" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2022-latest
+docker run --name mssql-docker -v ~/Biotonix/database/storage:/var/opt/mssql -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Omega123" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2022-latest 
